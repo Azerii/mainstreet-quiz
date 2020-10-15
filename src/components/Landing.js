8a29931e51474e761283f440b9ff32236225d3a1
user <ogidi58@gmail.com>
2020-10-15 13:12:31 +0100

    remove redirect from landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,12 +6,6 @@ import logo_main from '../assets/logo_main.png'
 
 
 export default function Landing() {
-    useEffect(() => {
-        setTimeout(() => {
-            window.location.replace('/quiz')
-        }, 2000)
-        //eslint-disable-next-line
-    }, [])
     return (
         <div className='landing-wrapper'>
             <div className='landing-inner'>
