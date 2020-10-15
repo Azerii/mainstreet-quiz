502d73bcfaafc3ce15573edb042f10ec362e26a1
user <ogidi58@gmail.com>
2020-10-15 13:47:35 +0100

    make UI mobile responsive

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -18,7 +18,7 @@ export default function Intro() {
     if (loading) {
         setTimeout(() => {
         setLoading(false)
-        }, 2000)
+        }, 3000)
         return <Landing />;
     }     
         
